0cfab22c9b9e9d81aaa00f8cde92f1d05cd5aea5
JD <jdbriones22@gmail.com>
2023-11-14 16:08:04 +1300

    Added Spotify Embed to About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Hero from "./pages/Hero";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Hero />
-    </>
+      <Projects />
+      <About />
+      <Contact />
+    </BrowserRouter>
   );
 }
 
