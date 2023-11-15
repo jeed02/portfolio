861666317719f2d654069a44c8f6185b727c899a
JD <jdbriones22@gmail.com>
2023-11-15 14:42:01 +1300

    Form skeleton finished

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,71 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
   return (
-    <div>
+    <motion.div>
       <h1 className="text-[4rem]">CONTACT ///</h1>
-    </div>
+      <form onSubmit={""}>
+        <label className="flex flex-col">
+          <span className="text-white text-[2rem] mb-4">Your Name</span>
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            placeholder="What's your name?"
+            className="bg-form-black py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium text-xl"
+          />
+        </label>
+        <label className="flex flex-col">
+          <span className="text-white text-[2rem] mb-4">Your email</span>
+          <input
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="What's your web address?"
+            className="bg-form-black py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium text-xl"
+          />
+        </label>
+        <label className="flex flex-col">
+          <span className="text-white text-[2rem] mb-4">Your Message</span>
+          <textarea
+            rows={7}
+            name="message"
+            value={form.message}
+            onChange={handleChange}
+            placeholder="What you want to say?"
+            className="bg-form-black py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium text-xl"
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="bg-bt-white py-1 px-8 rounded-xl outline-none w-fit text-bg-black my-6"
+        >
+          {loading ? "Sending..." : "Send"}
+        </button>
+      </form>
+    </motion.div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    colors: { "bg-black": "#151515", "grey-text": "#D6D6D6", white: "#fff" },
+    colors: {
+      "bg-black": "#151515",
+      "grey-text": "#D6D6D6",
+      white: "#fff",
+      "form-black": "#2f2f2f",
+      "bt-white": "#d9d9d9",
+    },
   },
   plugins: [],
 };
